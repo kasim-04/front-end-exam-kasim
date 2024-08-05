5f60aae15bd1c4c371b15ba5556dfea724af6389
kasim-04 <kasimchik04@gmail.com>
2024-08-05 03:09:09 +0300

    Выполненное задание

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import '../css/style.css';
+
+import image from './components/image';
+import intro from './components/intro';
+import languages from './components/languages';
+import experience from './components/experience';
+import tools from './components/tools';
+import education from './components/education';
+import interests from './components/interests';
+import contact from './components/contact';
+
+import { initSlider } from './slider';
+import { editableContentManage } from './editableManage';
+import { savePDF } from './savePDF';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <header class="header">
+        <button class="download-button text-1" id="download">Скачать PDF</button>
+    </header>
+    <main class="container" id="content-to-download">
+        ${image()}
+        ${intro()}
+        ${languages()}
+        ${experience()}
+        ${tools()}
+        ${education()}
+        <div class="interests-and-contact">
+            ${interests()}
+            ${contact()}
+        </div>
+    </main>
+`;
 
-setupCounter(document.querySelector('#counter'))
+editableContentManage()
+initSlider()
+savePDF()
